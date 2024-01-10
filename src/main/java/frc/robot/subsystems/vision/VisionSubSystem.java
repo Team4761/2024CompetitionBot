4a082049406b8ef425bd761e4d04ef980f7dbb0d
Mguarr <110061423+Mguarr@users.noreply.github.com>
2024-01-09 20:21:32 -0500

    Added toString

--- src/main/java/frc/robot/subsystems/vision/VisionSubSystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubSystem.java
@@ -6,13 +6,17 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import org.photonvision.PhotonCamera;
-// import org.photonvision.PhotonUtils;
+
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class VisionSubsystem extends SubsystemBase {
 
   //**ALL ANGLES IN RADIANS ALL DISTANCES IN METERS**//
-  //PhotonCamera mCamera;
+  PhotonCamera mCamera;
   private static final VisionSubsystem INSTANCE = new VisionSubsystem();
   public static VisionSubsystem getInstance(){
     return INSTANCE;
@@ -23,7 +27,7 @@ public class VisionSubsystem extends SubsystemBase {
   private boolean mDriverMode = false;
   public VisionSubsystem(){
     //Replace with name of cam
-    // mCamera = new PhotonCamera("Main");
+    mCamera = new PhotonCamera("Main"); 
   }
 
   @Override
@@ -32,37 +36,55 @@ public class VisionSubsystem extends SubsystemBase {
   }
 
 
-  // public int getBestTagID(){
-  //   return mCamera.getLatestResult().getBestTarget().getFiducialId();
-  // }
+   public int getBestTagID(){
+     return mCamera.getLatestResult().getBestTarget().getFiducialId();
+   }
 
-  // public boolean hasTargets(){
-  //   return mCamera.getLatestResult().hasTargets();
-  // }
-  // public Translation2d getTransDiff(double targetHeight){
-  //   if(hasTargets()){
-  //     double pitch = Units.degreesToRadians(mCamera.getLatestResult().getBestTarget().getPitch());
-  //     double distance = PhotonUtils.calculateDistanceToTargetMeters(
-  //       mCameraHeight, 
-  //       targetHeight, 
-  //       mCameraPitch,
-  //       pitch);
-  //     double yaw = Units.degreesToRadians(mCamera.getLatestResult().getBestTarget().getYaw());
-  //     double yDiff = distance * Math.cos(pitch);
-  //     double xDiff = yDiff / Math.cos(yaw) * Math.sin(yaw);
-  //     return new Translation2d(xDiff, yDiff - 1);
-  //   }
-  //   else{
-  //     return new Translation2d();
-  //   }
-  // }
+  public boolean hasTargets(){
+    return mCamera.getLatestResult().hasTargets();
+  }
+  public Translation2d getTransDiff(double targetHeight){
+    if(hasTargets()){
+      double pitch = Units.degreesToRadians(mCamera.getLatestResult().getBestTarget().getPitch());
+      double distance = PhotonUtils.calculateDistanceToTargetMeters(
+        mCameraHeight, 
+        targetHeight, 
+        mCameraPitch,
+        pitch);
+      double yaw = Units.degreesToRadians(mCamera.getLatestResult().getBestTarget().getYaw());
+      double yDiff = distance * Math.cos(pitch);
+      double xDiff = yDiff / Math.cos(yaw) * Math.sin(yaw);
+      return new Translation2d(xDiff, yDiff - 1);
+    }
+    else{
+      return new Translation2d();
+    }
+  }
 
-  // public void toggleDriverMode(){
-  //   mDriverMode = !mDriverMode;
-  //   mCamera.setDriverMode(mDriverMode);
-  // }
-  // //Returns false if not in driver mode
-  // public boolean getDriverMode(){
-  //   return mCamera.getDriverMode();
-  // }
+  public void toggleDriverMode(){
+    mDriverMode = !mDriverMode;
+    mCamera.setDriverMode(mDriverMode);
+  }
+  //Returns false if not in driver mode
+  public boolean getDriverMode(){
+    return mCamera.getDriverMode();
+  }
+
+  public String toString(){
+    var result = mCamera.getLatestResult();
+    if(mDriverMode){
+      return "Driver mode";
+    }
+    else if(result.hasTargets()){
+      String ids = "";
+      List<PhotonTrackedTarget> targets = result.getTargets();
+      for(PhotonTrackedTarget target : targets){
+        ids += target.getFiducialId();
+      }
+      return ids;
+    }
+    else{
+      return "No tag present";
+    }
+  }
 } 
