3e4bbacd44801b699c052da7eba7f1eee651fdf7
abuddies <91999625+abuddies0@users.noreply.github.com>
2024-02-11 16:57:35 -0500

    Added commands for testing to the shuffleboard.
    In the debug tab, there are commands for testing.

--- src/main/java/frc/robot/RobocketsShuffleboard.java
+++ src/main/java/frc/robot/RobocketsShuffleboard.java
@@ -2,13 +2,28 @@ package frc.robot;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Auto.AutoConstruct;
+import frc.robot.subsystems.climber.LoosenClimber;
+import frc.robot.subsystems.climber.TightenClimber;
+import frc.robot.subsystems.intake.FullIntake;
+import frc.robot.subsystems.leds.LedChargeUp;
+import frc.robot.subsystems.shooter.GetShooterToAngle;
+import frc.robot.subsystems.shooter.Shoot;
+import frc.robot.subsystems.shooter.ShootAtAngle;
+import frc.robot.subsystems.swerve.SwerveDriveStop;
+import frc.robot.subsystems.swerve.SwerveGoCartesianF;
+import frc.robot.subsystems.swerve.SwerveTurn;
+import frc.robot.subsystems.swerve.SwerveTurnTo;
 
 /**
  * <p> This is a handler class for everything that has to do with the shufflboard.
@@ -29,6 +44,8 @@ public class RobocketsShuffleboard {
     private ShuffleboardLayout settingsIntake;
     private ShuffleboardLayout settingsClimber;
     private ShuffleboardLayout settingsAuto;
+
+    private ShuffleboardLayout debugCommands;
 
     private ArrayList<SimpleWidget> settings;
 
@@ -52,8 +69,11 @@ public class RobocketsShuffleboard {
         settingsClimber = settingsTab.getLayout("Climber", BuiltInLayouts.kList).withSize(2,6).withPosition(6,0);
         settingsAuto = settingsTab.getLayout("Auto", BuiltInLayouts.kList).withSize(2,6).withPosition(8,0);
 
+        debugCommands = debugTab.getLayout("Commands", BuiltInLayouts.kList).withSize(2,6).withPosition(0,0);
+
         // Populate the layouts
         addSettings();
+        addCommands();
     }
 
     /**
@@ -75,6 +95,7 @@ public class RobocketsShuffleboard {
         // Intake settings
         addIntakeSetting("Intake Speed", 0.5);
         addIntakeSetting("Outtake Speed", 0.5);
+        addIntakeSetting("Full Intake Setting", 0.5);
 
         // Climber settings
         addClimberSetting("Climber Speed", 0.5);
@@ -82,6 +103,33 @@ public class RobocketsShuffleboard {
         // Auto settings
         shuffleboard.settingsAuto.add("Auto", new AutoConstruct()); // Cannot be found using the getSettingNum function. Must use the AutoConstruct.scheduleSelectedCommand() method.
     }
+
+    /**
+     * <p> Adds all the command for testing to the debug tab.
+     */
+    public void addCommands() {
+        // Swerve commands
+        addCommand("Go 1 Meter Forward", new SwerveGoCartesianF(Robot.getMap().swerve, new Translation2d(1,0)));
+        addCommand("Go 1 Meter Left", new SwerveGoCartesianF(Robot.getMap().swerve, new Translation2d(0,1)));
+        addCommand("Turn 90 Degrees", new SwerveTurn(Robot.getMap().swerve, new Rotation2d(Units.degreesToRadians(90))));
+        addCommand("Turn To 90 Degrees", new SwerveTurnTo(Robot.getMap().swerve, new Rotation2d(Units.degreesToRadians(90))));
+
+        // Shooter commands
+        addCommand("Shoot", new Shoot(getSettingNum("Shooter Out Speed")));
+        addCommand("Turn Shooter To 45 Degrees", new GetShooterToAngle(Units.degreesToRadians(45)));
+        addCommand("Shoot At 45 Degrees", new ShootAtAngle(Units.degreesToRadians(45), getSettingNum("Shooter Out Speed")));
+        
+        // Intake settings
+        addCommand("Full Intake", new FullIntake(getSettingNum("Full Intake Speed"), Robot.getMap()));
+
+        // LED commands
+        addCommand("LED Charge Up", new LedChargeUp(3000, Robot.getMap().leds));
+
+        // Climber commands
+        addCommand("Loosen Climber", new LoosenClimber());
+        addCommand("Tighten Climber", new TightenClimber());
+    }
+
 
     /**
      * <p> Adds any specific info that can only be accessed during teleop.
@@ -128,6 +176,16 @@ public class RobocketsShuffleboard {
                 return settings.get(i).getEntry().getDouble(0.0);
         }
         return 0.0;
+    }
+
+
+    /**
+     * <p> Adds a command to the command layout in the debug tab that can be activated by pressing it.
+     * @param command The command to be added.
+     * @param key The name of the command.
+     */
+    public void addCommand(String key, Command command) {
+        shuffleboard.debugCommands.add(key, command);
     }
 
 }
