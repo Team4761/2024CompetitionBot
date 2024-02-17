0593ecbf6693fd54d79c784fa596a259f16bc607
Erik Currin <ehc12345678@gmail.com>
2024-02-16 19:27:53 -0500

    Fixed charge up

--- src/main/java/frc/robot/RobocketsShuffleboard.java
+++ src/main/java/frc/robot/RobocketsShuffleboard.java
@@ -123,7 +123,7 @@ public class RobocketsShuffleboard {
         addCommand("Full Intake", new FullIntake(getSettingNum("Full Intake Speed"), Robot.getMap()));
 
         // LED commands
-        // addCommand("LED Charge Up", new LedChargeUp(3000, Robot.getMap().leds)); DOESN'T WORK RN
+        addCommand("LED Charge Up", new LedChargeUp(3000, Robot.getMap()));
 
         // Climber commands
         addCommand("Loosen Climber", new LoosenClimber());
